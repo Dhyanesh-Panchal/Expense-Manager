7e949824bd756f9196f6a13174f9ba986213b544
Dhyanesh Panchal <dhynincoding2004@gmail.com>
2023-12-23 20:42:44 +0530

    Tried to introduce PWA

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// vite.config.js or vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt'],
+      manifest: {
+        name: 'Expense Manager',
+        short_name: 'Expenses',
+        description: 'Manage your expenses on the go!',
+        theme_color: '#4caf50',
+        icons: [
+          {
+            src: 'pwa-192x192.png', // 192x192px
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png', // 512x512px
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          // ... you can include other sizes as needed
+        ],
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+      },
+    }),
+  ],
+});
